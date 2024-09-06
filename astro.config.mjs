9b7331b17f2e1de69798829bda97c89023ed7ede
tom-wat <52376398+tom-wat@users.noreply.github.com>
2024-09-07 03:18:15 +0900

    不要なコードの削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import icon from "astro-icon";
 
 import relativeLinks from "astro-relative-links";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://tom-wat.github.io/",
   base: "/sunshine-solutions",
