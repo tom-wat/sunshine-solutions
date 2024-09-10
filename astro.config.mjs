9bded8847f3b3ec2f09be0a9b55e0846b2511785
tom-wat <52376398+tom-wat@users.noreply.github.com>
2024-09-10 18:28:49 +0900

    設定の変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
-
-import relativeLinks from "astro-relative-links";
 
 export default defineConfig({
-  site: "https://tom-wat.github.io/",
-  base: "/sunshine-solutions",
-  integrations: [icon(), relativeLinks()],
+  site: "https://sunshine-solutions.vercel.app/",
+  base: "/",
 });
